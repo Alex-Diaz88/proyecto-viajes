1cec17871a3eb7ecc5523f9726f3910b81225f19
JoseSoloaga <josesoloagam@gmail.com>
2022-09-20 12:16:28 +0200

    Avatar-NewTravelForm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import NewTravelForm from "./components/NewTravelForm/index";
+import { CustomTokenContextProvider } from "./contexts/TokenContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+  <BrowserRouter>
+  <CustomTokenContextProvider>
+    <NewTravelForm/>
+    </CustomTokenContextProvider>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
---
+++ frontend/src/contexts/TokenContext.js
@@ -0,0 +1,62 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+export const TokenContext = createContext();
+
+export const CustomTokenContextProvider = ({ children }) => {
+  const [token, setToken] = useLocalStorage("token");
+  const [loggedUser, setLoggedUser] = useState({});
+
+  useEffect(() => {
+    if (!token) {
+      setLoggedUser({});
+      return;
+    }
+
+    console.log("1");
+    const fetchUser = async () => {
+      try {
+        const decodedToken = JSON.parse(atob(token.split(".")[1]));
+
+        console.log("2");
+
+        const res = await fetch(
+          `${process.env.REACT_APP_API_URL}/users/${decodedToken.id}`
+        );
+
+        const body = await res.json();
+
+        console.log("3");
+
+        if (!res.ok) {
+          console.log("4");
+          throw new Error(body.message);
+        }
+
+        setLoggedUser(body.data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchUser();
+  }, [token]);
+
+  return (
+    <TokenContext.Provider
+      value={{ token, setToken, loggedUser, setLoggedUser }}
+    >
+      {children}
+    </TokenContext.Provider>
+  );
+};
+
+export const useTokenContext = () => {
+  const { token, setToken, loggedUser, setLoggedUser } =
+    useContext(TokenContext);
+
+  return { token, setToken, loggedUser, setLoggedUser };
+
+  // Forma abreviada:
+  // return useContext(TokenContext);
+};
