fda310f32165e34d57d18ced7a0029ca6a34ec20
AlexDiaz <xandre88@gmail.com>
2022-09-21 10:58:33 +0200

    componentes travel/travelList

---
+++ frontend/src/components/TravelList/index.js
@@ -0,0 +1,18 @@
+import Travel from "../Travel";
+import "./styles.css";
+
+const TravelList = ({ travels }) => {
+  return (
+    <ul className="travel_list">
+      {travels.map((travel) => {
+        return (
+          <li key={travel.id}>
+            <Travel travel={travel} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default TravelList;
