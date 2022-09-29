e5c3337daea60224b1a5529b4261773a9755c14f
Carles <carles.ribas.costa@gmail.com>
2022-09-29 19:34:14 +0200

    enlace-perfil

--- frontend/src/components/CommentList/index.js
+++ frontend/src/components/CommentList/index.js
@@ -4,7 +4,7 @@ import Comment from "../Comment";
 const CommentList = ({ comments }) => {
   return (
     <ul className="comment_list">
-      {comments.map((comment) => {
+      {comments?.map((comment) => {
         return (
           <li key={comment.id}>
             <Comment comment={comment} />
