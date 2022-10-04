9e0fcfc8554e6b865eb35fdda0172222561698b4
Carles <carles.ribas.costa@gmail.com>
2022-10-04 12:03:43 +0200

    error-nuevo-viaje

--- frontend/src/components/NewTravelButton/index.js
+++ frontend/src/components/NewTravelButton/index.js
@@ -1,19 +1,29 @@
 import "./styles.css";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useTokenContext } from "../../contexts/TokenContext";
 
 const NewTravelButton = () => {
+  const { token } = useTokenContext();
   const navigate = useNavigate();
+
+  const newTravelfunction = () => {
+    try {
+      if (!token) {
+        throw new Error("Debes iniciar sesión para compartir un nuevo viaje");
+      }
+
+      navigate(`/travels/new`);
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    }
+  };
 
   return (
     <div className="new-travel-button">
-      <button
-        onClick={() => {
-          navigate(`/travels/new`);
-        }}
-      >
-        Añadir Viaje
-      </button>
+      <button onClick={newTravelfunction}>Añadir Viaje</button>
     </div>
   );
 };
